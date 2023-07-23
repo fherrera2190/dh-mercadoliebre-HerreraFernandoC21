c038f7251d5b54dcda4a3e5592c4ddf1f82b7d74
Fernando Herrera <herrera.fernando1209@gmail.com>
2023-07-23 03:12:27 -0300

    add section register and login

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.get('/', (req, res) => {
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'register.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', '404.html'));
